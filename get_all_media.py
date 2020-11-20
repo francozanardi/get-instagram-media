35a7a3ff4fc8d8e4e16b6cecaaec7ed22a90afb3
Franco Zanardi <francozanardi.97@gmail.com>
2020-11-20 15:46:15 -0300

    Modificamos el código por uno más genérico.

--- get_all_media.py
+++ get_all_media.py
@@ -13,11 +13,11 @@ def get_id(username):
 
 	return "";
 	
-def save_file(link, path, reintentos, sleep_error):
+def save_file(link, path, reintentos = 3, sleep_error = 10):
 	reintentos_actuales = 0
-	finalizado = False
+	descargado = False
 	
-	while(not finalizado and reintentos_actuales < reintentos):
+	while(not descargado and reintentos_actuales < reintentos):
 		resp_media = get(link)
 
 		if(resp_media.status_code == 200):
@@ -25,77 +25,39 @@ def save_file(link, path, reintentos, sleep_error):
 			img.write(resp_media.content)
 			img.close()
 			
-			finalizado = True
+			descargado = True
 		else:
 			reintentos_actuales += 1
 			sleep(sleep_error)
 			
 		
-	return reintentos_actuales < reintentos
-
-#A partir del ID del usuario 'id' obtenemos su contenido multimedia y lo guardamos en la carpeta 'folder_name'.
-def get_all_media(id, folder_name, sleep_requests = 5, sleep_error = 10, reintentos = 3):
-	has_next_page = True
-	after = None
-	reintentos_actuales = 0
-	nro_media = 0
+	return descargado
 	
-	variables = {
-		"id": id,
-		"first": 50,
-		"after": after
-	}
+def ig_request(hash_id, variables, resolver, resolver_args = {}, cookies = {}, sleep_requests = 5, sleep_error = 10, reintentos = 3):
+	has_next_page = True
+	reintentos_actuales = 0
 	
 	params = {
-		"query_hash": "003056d32c2554def87228bc3fd9668a",
+		"query_hash": hash_id,
 		"variables": dumps(variables)
 	}
 	
 	while(has_next_page and reintentos_actuales < reintentos):
-		resp = get("https://www.instagram.com/graphql/query/", params = params)
+		resp = get("https://www.instagram.com/graphql/query/", params = params, cookies = cookies)
 		
 		if(resp.status_code == 200):
 			reintentos_actuales = 0
-			data_resp = resp.json()["data"]["user"]["edge_owner_to_timeline_media"]
-			
-			#iteramos sobre los elementos multimedia y los guardamos
-			for node in data_resp["edges"]:
-				if(node["node"]["__typename"] == "GraphSidecar"): #es un sidecar
-					nro_img = 0
-					for img in node["node"]["edge_sidecar_to_children"]["edges"]:
-						is_save = save_file(img["node"]["display_url"], os.path.join(folder_name, str(nro_media) + "_" + str(nro_img) + ".jpg"), reintentos, sleep_error)
-						
-						if(not is_save):
-							reintentos_actuales = reintentos
-							break
-							
-						nro_img += 1
-				else:
-					ext = ".jpg"
-					link = "display_url"
-					
-					if(node["node"]["__typename"] == "GraphVideo"):
-						ext = ".mp4"
-						link = "video_url"
-						
-					is_save = save_file(node["node"][link], os.path.join(folder_name, str(nro_media) + ext), reintentos, sleep_error)
-					
-					if(not is_save):
-						reintentos_actuales = reintentos
-						break
-				
-				nro_media += 1
-				print("(%d/%s)" % (nro_media, data_resp["count"]))
-				
-			
 
-			has_next_page = data_resp["page_info"]["has_next_page"]
-			
-			if(has_next_page):
-				variables["after"] = data_resp["page_info"]["end_cursor"]
-				params["variables"] = dumps(variables)
+			try:
+				has_next_page = resolver(variables, resp.json(), resolver_args);
 				
-				sleep(sleep_requests)
+				if(has_next_page):
+					params["variables"] = dumps(variables)
+					sleep(sleep_requests)
+					
+			except Exception as err:
+				print("Se prudujo un error en el resolver:", err)
+				reintentos_actuales = reintentos #si se produjo un error salimos del bucle inesperadamente
 			
 		else:
 			reintentos_actuales += 1
@@ -106,7 +68,61 @@ def get_all_media(id, folder_name, sleep_requests = 5, sleep_error = 10, reinten
 			
 	
 	return reintentos_actuales < reintentos
+
+
+nro_media = 0 #variable global para sumar la cantidad de media descargada hasta el momento
+def resolver_media(variables, data_resp, extra_args):
+	global nro_media
+	
+	folder_name = extra_args["folder_name"]
+	data_resp = data_resp["data"]["user"]["edge_owner_to_timeline_media"]
+	
+	#iteramos sobre los elementos multimedia y los guardamos
+	for node in data_resp["edges"]:
+		node = node["node"]
 		
+		if(node["__typename"] == "GraphSidecar"): #es un sidecar
+			nro_img = 0
+			for img in node["edge_sidecar_to_children"]["edges"]:
+				is_save = save_file(img["node"]["display_url"], os.path.join(folder_name, str(nro_media) + "_" + str(nro_img) + ".jpg"))
+				
+				if(not is_save):
+					raise Exception("Error al guardar el elemendo %d (de un Sidecar la imagen %d)." % (nro_media, nro_img))
+					
+				nro_img += 1
+		else:
+			ext = ".jpg"
+			link = "display_url"
+			
+			if(node["__typename"] == "GraphVideo"):
+				ext = ".mp4"
+				link = "video_url"
+				
+			is_save = save_file(node[link], os.path.join(folder_name, str(nro_media) + ext))
+			
+			if(not is_save):
+				raise Exception("Error al guardar el elemendo %d." % (nro_media))
+		
+		nro_media += 1
+		print("(%d/%s)" % (nro_media, data_resp["count"]))
+
+	if(data_resp["page_info"]["has_next_page"]):
+		variables["after"] = data_resp["page_info"]["end_cursor"]
+		return True
+		
+	return False
+	
+def get_all_media(id, folder_name):	
+	variables = {
+		"id": id,
+		"first": 50
+	}
+	
+	extra = {
+		"folder_name": folder_name
+	}
+	
+	return ig_request("003056d32c2554def87228bc3fd9668a", variables, resolver_media, resolver_args = extra)
 		
 
 username = input("Username: ")
